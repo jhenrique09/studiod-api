c39235b1ab91cf7bcfac4887eff0d022eaf2d0e4
jhenrique09 <jhsv09@gmail.com>
2022-07-14 23:08:00 -0300

    Reformata migration de usuarios

--- src/database/migrations/1657684487146-TabelaUsuarios.ts
+++ src/database/migrations/1657684487146-TabelaUsuarios.ts
@@ -5,7 +5,21 @@ export class TabelaUsuarios1657684487146 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "usuarios" ("id" SERIAL NOT NULL, "nome" character varying(500) NOT NULL, "email" character varying NOT NULL, "senha" character varying NOT NULL, "senha_uso_unico" character varying, "requer_atualizacao_senha" boolean NOT NULL DEFAULT false, "telefone" character varying(11) NOT NULL, "data_criacao" TIMESTAMP NOT NULL DEFAULT now(), "data_atualizacao" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_446adfc18b35418aac32ae0b7b5" UNIQUE ("email"), CONSTRAINT "UQ_a74e0e525737469ee0a512505fe" UNIQUE ("telefone"), CONSTRAINT "PK_d7281c63c176e152e4c531594a8" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "usuarios"
+         (
+           "id"                       SERIAL                 NOT NULL,
+           "nome"                     character varying(500) NOT NULL,
+           "email"                    character varying      NOT NULL,
+           "senha"                    character varying      NOT NULL,
+           "senha_uso_unico"          character varying,
+           "requer_atualizacao_senha" boolean                NOT NULL DEFAULT false,
+           "telefone"                 character varying(11)  NOT NULL,
+           "data_criacao"             TIMESTAMP              NOT NULL DEFAULT now(),
+           "data_atualizacao"         TIMESTAMP              NOT NULL DEFAULT now(),
+           CONSTRAINT "UQ_446adfc18b35418aac32ae0b7b5" UNIQUE ("email"),
+           CONSTRAINT "UQ_a74e0e525737469ee0a512505fe" UNIQUE ("telefone"),
+           CONSTRAINT "PK_d7281c63c176e152e4c531594a8" PRIMARY KEY ("id")
+         )`,
     );
   }
 
